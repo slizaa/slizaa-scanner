90eba9ee802bae0e0dd520151fea68ffc2c77512
Gerd Wütherich <gerd@gerd-wuetherich.de>
2017-06-16 14:34:56 +0200

    ~ minor change

--- org.slizaa.scanner/org.slizaa.scanner.model/src/main/java/org/slizaa/scanner/model/resource/IDirectoryNode.java
+++ org.slizaa.scanner/org.slizaa.scanner.model/src/main/java/org/slizaa/scanner/model/resource/IDirectoryNode.java
@@ -1,5 +1,20 @@
 package org.slizaa.scanner.model.resource;
 
 public interface IDirectoryNode {
+  
+  /**
+   * <p>
+   * The full path of the directory, e.g. <code>'org/example'</code>. Note that resource paths are always
+   * slash-delimited ('/').
+   * </p>
+   */
+  public static final String PROPERTY_PATH               = "path";
 
+  /**
+   * <p>
+   * the root directory or archive file that contains the resource (e.g. <code>'c:/dev/classes.zip'</code> or
+   * <code>'c:/dev/source'</code>). Note that resource paths are always slash-delimited ('/').
+   * </p>
+   */
+  public static final String PROPERTY_ROOT               = "root";
 }
