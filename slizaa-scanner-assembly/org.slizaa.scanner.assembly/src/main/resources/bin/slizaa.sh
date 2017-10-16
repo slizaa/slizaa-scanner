fa6dd069f695e820d1608111935ad36629c78de7
Gerd Wütherich <gerd@gerd-wuetherich.de>
2017-10-16 15:10:49 +0200

    ~ removed cmd

--- slizaa-scanner-assembly/org.slizaa.scanner.assembly/src/main/resources/bin/slizaa.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if [ -z "$SLIZAA_ROOT" ] ; then
-  SCRIPT_DIR=`dirname "$0"`
-  export SLIZAA_ROOT=`cd "$SCRIPT_DIR/.." && pwd -P`
-fi
-LIBS_DIR=$SLIZAA_ROOT/libs
-java %SLIZAA_OPTS% -cp %LIBS_DIR%\* org.slizaa.scanner.cmdline.Slizaa %*
-
