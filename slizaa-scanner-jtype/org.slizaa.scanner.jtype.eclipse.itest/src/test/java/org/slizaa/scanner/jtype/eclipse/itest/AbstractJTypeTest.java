307a3bd3810bcaf3b36381339a61209881331105
Gerd Wütherich <gerd@gerd-wuetherich.de>
2017-10-17 16:54:30 +0200

    introduced JType itest

---
+++ slizaa-scanner-jtype/org.slizaa.scanner.jtype.eclipse.itest/src/test/java/org/slizaa/scanner/jtype/eclipse/itest/AbstractJTypeTest.java
@@ -0,0 +1,105 @@
+package org.slizaa.scanner.jtype.eclipse.itest;
+
+import static org.ops4j.pax.exam.CoreOptions.bootDelegationPackages;
+import static org.ops4j.pax.exam.CoreOptions.bundle;
+import static org.ops4j.pax.exam.CoreOptions.junitBundles;
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.CoreOptions.options;
+import static org.ops4j.pax.exam.CoreOptions.wrappedBundle;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.inject.Inject;
+
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.MavenUtils;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerMethod;
+import org.ops4j.pax.url.mvn.MavenResolvers;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.BundleException;
+
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerMethod.class)
+public abstract class AbstractJTypeTest {
+
+  {
+    System.setProperty("org.ops4j.pax.url.mvn.localRepository",
+        System.getProperty("user.home") + File.separator + ".m2" + File.separator + "repository");
+  }
+
+  /** - */
+  @Inject
+  private BundleContext bundleContext;
+
+  /**
+   * <p>
+   * </p>
+   *
+   * @return
+   */
+  public BundleContext bundleContext() {
+    return bundleContext;
+  }
+
+  /**
+   * <p>
+   * </p>
+   *
+   * @return
+   * @throws IOException
+   */
+  @Configuration
+  public Option[] config() throws IOException {
+
+    // resolve jtype version
+    String jtypeVersion = MavenUtils.getArtifactVersion("org.slizaa.scanner.jtype", "org.slizaa.scanner.jtype");
+
+    // resolve the jtype artifact
+    File jtypeFile = MavenResolvers.createMavenResolver(null, null).resolve("org.slizaa.scanner.jtype",
+        "org.slizaa.scanner.jtype", null, null, jtypeVersion);
+
+    //
+    return options(
+
+        //
+        bootDelegationPackages("sun.*", "com.sun.*"),
+
+        // add the test dependencies
+        mavenBundle("org.assertj", "assertj-core").versionAsInProject(), junitBundles(),
+
+        //
+        mavenBundle("com.google.guava", "guava").versionAsInProject(),
+
+        mavenBundle("org.eclipse.platform", "org.eclipse.equinox.common").versionAsInProject(),
+        mavenBundle("com.google.code.gson", "gson").versionAsInProject(),
+        mavenBundle("org.ops4j.pax.url", "pax-url-aether").versionAsInProject(),
+
+        //
+        wrappedBundle(mavenBundle("org.neo4j.driver", "neo4j-java-driver").versionAsInProject()),
+
+        //
+        mavenBundle("org.slizaa.scanner.core", "org.slizaa.scanner.core.spi-api").versionAsInProject(),
+        mavenBundle("org.slizaa.scanner.core", "org.slizaa.scanner.core.contentdefinition").versionAsInProject(),
+        mavenBundle("org.slizaa.scanner.core", "org.slizaa.scanner.core.classpathscanner").versionAsInProject(),
+        mavenBundle("org.slizaa.scanner.neo4j", "org.slizaa.scanner.neo4j.eclipse").versionAsInProject().start(),
+        bundle("reference:" + jtypeFile.toURI().toString()).start());
+  }
+
+  /**
+   * <p>
+   * </p>
+   * 
+   * @throws BundleException
+   */
+  protected void startAllBundles() throws BundleException {
+    for (Bundle bundle : bundleContext.getBundles()) {
+      bundle.start();
+    }
+  }
+}
