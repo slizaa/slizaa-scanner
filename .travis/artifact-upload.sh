9b01414e8c207642485051928bc2aba8edef88f0
Gerd Wuetherich <gerd@gerd-wuetherich.de>
2018-05-01 07:44:06 +0200

    ~ changed repo creation to 'org.reficio.p2-maven-plugin'

--- .travis/artifact-upload.sh
+++ .travis/artifact-upload.sh
@@ -5,7 +5,7 @@ target_dir=slizaa-scanner-neo4j
 
 echo TRAVIS_BUILD_DIR: $TRAVIS_BUILD_DIR
 
-PRODUCTS_DIR=$TRAVIS_BUILD_DIR/slizaa-scanner-neo4j-eclipse/org.slizaa.scanner.neo4j.eclipse.repository/target/repository
+PRODUCTS_DIR=$TRAVIS_BUILD_DIR/org.slizaa.scanner.neo4j.eclipse.repo/target/repository
 echo PRODUCTS_DIR: $PRODUCTS_DIR
 
 filesToUpload=$(find $PRODUCTS_DIR -name "*.jar" -printf '%P\n')
