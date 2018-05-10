84798206c6e7c3ef9ef553d0b05ba6fc26805df3
Gerd Wuetherich <gerd@gerd-wuetherich.de>
2018-05-10 23:17:34 +0200

    ~ support for Procedures and UserFunction

--- org.slizaa.scanner.neo4j.graphdbfactory/src/main/java/org/slizaa/scanner/neo4j/graphdbfactory/GraphDbFactory.java
+++ org.slizaa.scanner.neo4j.graphdbfactory/src/main/java/org/slizaa/scanner/neo4j/graphdbfactory/GraphDbFactory.java
@@ -1,18 +1,15 @@
 /*******************************************************************************
  * Copyright (C) 2017 Gerd Wuetherich
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
  ******************************************************************************/
 package org.slizaa.scanner.neo4j.graphdbfactory;
 
@@ -20,8 +17,11 @@ import static com.google.common.base.Preconditions.checkNotNull;
 
 import java.io.File;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
@@ -37,14 +37,38 @@ import org.slizaa.scanner.core.api.graphdb.IGraphDbFactory;
 public class GraphDbFactory implements IGraphDbFactory {
 
   /** - */
-  public static final String SLIZAA_NEO4J_EXTENSIONCLASSES = "slizaa.neo4j.extensionclasses";
+  public static final String       SLIZAA_NEO4J_EXTENSIONCLASSES = "slizaa.neo4j.extensionclasses";
+
+  /** - */
+  private Supplier<List<Class<?>>> _databaseExtensionsSupplier;
+
+  /**
+   * <p>
+   * Creates a new instance of type {@link GraphDbFactory}.
+   * </p>
+   *
+   */
+  public GraphDbFactory() {
+    this(null);
+  }
+
+  /**
+   * <p>
+   * Creates a new instance of type {@link GraphDbFactory}.
+   * </p>
+   *
+   * @param databaseExtensionsSupplier
+   */
+  public GraphDbFactory(Supplier<List<Class<?>>> databaseExtensionsSupplier) {
+    this._databaseExtensionsSupplier = databaseExtensionsSupplier;
+  }
 
   /**
    * {@inheritDoc}
    */
   @Override
   public IGraphDbBuilder newGraphDb(int port, File storeDir) {
-    return new GraphDbBuilder(port, storeDir);
+    return new GraphDbBuilder(port, storeDir, this._databaseExtensionsSupplier);
   }
 
   /**
@@ -56,16 +80,19 @@ public class GraphDbFactory implements IGraphDbFactory {
   private class GraphDbBuilder implements IGraphDbBuilder {
 
     /** - */
-    private int                 _port;
+    private int                      _port;
 
     /** - */
-    private File                _storeDir;
+    private File                     _storeDir;
 
     /** - */
-    private Object              _userObject;
+    private Object                   _userObject;
 
     /** - */
-    private Map<String, Object> _configuration = new HashMap<>();
+    private Map<String, Object>      _configuration = new HashMap<>();
+
+    /** - */
+    private Supplier<List<Class<?>>> _databaseExtensionsSupplier;
 
     /**
      * <p>
@@ -76,8 +103,21 @@ public class GraphDbFactory implements IGraphDbFactory {
      * @param storeDir
      */
     public GraphDbBuilder(int port, File storeDir) {
-      _port = port;
-      _storeDir = checkNotNull(storeDir);
+      this(port, storeDir, null);
+    }
+
+    /**
+     * <p>
+     * Creates a new instance of type {@link GraphDbBuilder}.
+     * </p>
+     *
+     * @param port
+     * @param storeDir
+     */
+    public GraphDbBuilder(int port, File storeDir, Supplier<List<Class<?>>> databaseExtensionsSupplier) {
+      this._port = port;
+      this._storeDir = checkNotNull(storeDir);
+      this._databaseExtensionsSupplier = databaseExtensionsSupplier;
     }
 
     /**
@@ -87,7 +127,7 @@ public class GraphDbFactory implements IGraphDbFactory {
     public <T> IGraphDbBuilder withUserObject(T userObject) {
 
       //
-      _userObject = userObject;
+      this._userObject = userObject;
 
       //
       return this;
@@ -100,7 +140,7 @@ public class GraphDbFactory implements IGraphDbFactory {
     public IGraphDbBuilder withConfiguration(String key, Object value) {
 
       //
-      _configuration.put(key, value);
+      this._configuration.put(key, value);
 
       //
       return this;
@@ -118,39 +158,66 @@ public class GraphDbFactory implements IGraphDbFactory {
       BoltConnector bolt = new BoltConnector("0");
 
       //
-      GraphDatabaseService graphDatabase = factory.newEmbeddedDatabaseBuilder(_storeDir)
+      GraphDatabaseService graphDatabase = factory.newEmbeddedDatabaseBuilder(this._storeDir)
           .setConfig(bolt.type, ConnectorType.BOLT.name()).setConfig(bolt.enabled, "true")
-          .setConfig(bolt.listen_address, "localhost:" + _port).setConfig(bolt.encryption_level, "DISABLED")
+          .setConfig(bolt.listen_address, "localhost:" + this._port).setConfig(bolt.encryption_level, "DISABLED")
           .newGraphDatabase();
 
-      //
-      Object dbExtensions = _configuration.get(SLIZAA_NEO4J_EXTENSIONCLASSES);
+      registerDatabaseExtensions(graphDatabase);
+
+      // the Neo4jGraphDb
+      return new Neo4jGraphDb(graphDatabase, this._port, this._userObject);
+    }
+
+    /**
+     * <p>
+     * </p>
+     *
+     * @param graphDatabase
+     */
+    private void registerDatabaseExtensions(GraphDatabaseService graphDatabase) {
 
       //
+      List<Class<?>> extensionsToRegister = new LinkedList<>();
+
+      //
+      Object dbExtensions = this._configuration.get(SLIZAA_NEO4J_EXTENSIONCLASSES);
       if (dbExtensions != null && dbExtensions instanceof List && !((List<?>) dbExtensions).isEmpty()) {
 
-        //
-        Procedures proceduresService = ((GraphDatabaseAPI) graphDatabase).getDependencyResolver()
-            .resolveDependency(Procedures.class);
+        // extract the classes...
+        List<Class<?>> classes = ((List<?>) dbExtensions).stream().filter(e -> e instanceof Class<?>)
+            .map(e -> (Class<?>) e).collect(Collectors.toList());
 
-        //
-        for (Object element : ((List<?>) dbExtensions)) {
+        // ...and register them
+        extensionsToRegister.addAll(classes);
+      }
 
-          if (element instanceof Class<?>) {
-            try {
-              proceduresService.registerFunction((Class<?>) element);
-              proceduresService.registerProcedure((Class<?>) element);
-            } catch (KernelException e) {
-              // TODO Auto-generated catch block
-              e.printStackTrace();
-            }
-          }
+      //
+      if (this._databaseExtensionsSupplier != null) {
+
+        // extract the classes...
+        List<Class<?>> classes = this._databaseExtensionsSupplier.get();
+
+        // ...and register them
+        if (classes != null) {
+          extensionsToRegister.addAll(classes);
         }
       }
 
-      // the Neo4jGraphDb
-      return new Neo4jGraphDb(graphDatabase, _port, _userObject);
+      // get the procedure service
+      Procedures proceduresService = ((GraphDatabaseAPI) graphDatabase).getDependencyResolver()
+          .resolveDependency(Procedures.class);
+
+      // register all elements
+      for (Class<?> element : extensionsToRegister) {
+        try {
+          proceduresService.registerFunction(element);
+          proceduresService.registerProcedure(element);
+        } catch (KernelException e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
+        }
+      }
     }
   }
-
 }
--- org.slizaa.scanner.neo4j.osgi/src/main/java/org/slizaa/scanner/neo4j/osgi/internal/Activator.java
+++ org.slizaa.scanner.neo4j.osgi/src/main/java/org/slizaa/scanner/neo4j/osgi/internal/Activator.java
@@ -1,25 +1,29 @@
 /*******************************************************************************
  * Copyright (C) 2017 Gerd Wuetherich
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
  ******************************************************************************/
 package org.slizaa.scanner.neo4j.osgi.internal;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import org.neo4j.procedure.Procedure;
+import org.neo4j.procedure.UserFunction;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.util.tracker.ServiceTracker;
 import org.slizaa.scanner.core.api.graphdb.IGraphDbFactory;
 import org.slizaa.scanner.core.api.importer.IModelImporterFactory;
+import org.slizaa.scanner.core.classpathscanner.IClasspathScannerService;
 import org.slizaa.scanner.neo4j.graphdbfactory.GraphDbFactory;
 import org.slizaa.scanner.neo4j.importer.ModelImporterFactory;
 
@@ -27,12 +31,32 @@ import org.slizaa.scanner.neo4j.importer.ModelImporterFactory;
  */
 public class Activator implements BundleActivator {
 
+  /** - */
+  private ServiceTracker<IClasspathScannerService, IClasspathScannerService> _classpathScannerService;
+
   @Override
   public void start(BundleContext context) throws Exception {
 
+    this._classpathScannerService = new ServiceTracker<>(context, IClasspathScannerService.class, null);
+    this._classpathScannerService.open();
+
     //
     context.registerService(IModelImporterFactory.class.getName(), new ModelImporterFactory(), null);
-    context.registerService(IGraphDbFactory.class.getName(), new GraphDbFactory(), null);
+    context.registerService(IGraphDbFactory.class.getName(), new GraphDbFactory(() -> {
+
+      //
+      IClasspathScannerService classpathScannerService = this._classpathScannerService.getService();
+
+      //
+      if (classpathScannerService != null) {
+        List<Class<?>> result = new LinkedList<>();
+        result.addAll(classpathScannerService.getExtensionsWithMethodAnnotation(Procedure.class));
+        result.addAll(classpathScannerService.getExtensionsWithMethodAnnotation(UserFunction.class));
+        return result;
+      }
+      return null;
+
+    }), null);
   }
 
   /**
@@ -40,5 +64,6 @@ public class Activator implements BundleActivator {
    */
   @Override
   public void stop(BundleContext context) throws Exception {
+    this._classpathScannerService.close();
   }
 }
